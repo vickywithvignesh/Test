4c9a833ac5a17fe2692f9407df06ee0e26f3a57e
Vicky Vignesh <vickyvignesh1091@gmail.com>
2018-03-27 12:27:45 +0530

    vhanges

--- node.js
+++ node.js
@@ -12,37 +12,7 @@ var app = express();
 
 app.get('/tokens',function(request,response)
 {
-var AccessToken = require('twilio').jwt.AccessToken;
-var VideoGrant = AccessToken.VideoGrant;
-var ACCOUNT_SID = 'ACe68dc19a5cea48001c9982380dc9d1f3';
-var API_KEY_SID = 'SK79aef9d3ce2c59b3d3821ec1323eb9ce';
-var API_KEY_SECRET = 'YaHVcP4israaCjAQZh1XklQE5jemBfXQ';
-var r=Math.random();
-// Substitute your Twilio AccountSid and ApiKey details
+response.send("hi");
 
-
-// Create an Access Token
-var accessToken = new AccessToken(
-  ACCOUNT_SID,
-  API_KEY_SID,
-  API_KEY_SECRET
-);
-
-
-// Set the Identity of this token
- 
-accessToken.identity =""+r;
-/*var r=str.toString();
-accessToken.identity ='(str+1)'+r;
-str++;*/
-// Grant access to Video
-var grant = new VideoGrant();
-grant.room = 'myyyroom';
-accessToken.addGrant(grant);
-
-// Serialize the token as a JWT
-var jwt = accessToken.toJwt();
-console.log(jwt);
-response.end(jwt);
 })
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
