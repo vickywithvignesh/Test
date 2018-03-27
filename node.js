e4c798924159527d10525447172b18e9f63e6891
Vicky Vignesh <vickyvignesh1091@gmail.com>
2018-03-27 11:32:13 +0530

    yes

--- node.js
+++ node.js
@@ -1,6 +1,6 @@
-var http = require('http');
+const express = require('express')
+const app = express()
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(8080);
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
