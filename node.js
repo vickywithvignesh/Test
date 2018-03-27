029c0ca9aee0b51359105891e44b192a32cd766f
Vicky Vignesh <vickyvignesh1091@gmail.com>
2018-03-27 11:39:44 +0530

    yes

--- node.js
+++ node.js
@@ -1,6 +1,22 @@
-const express = require('express')
-const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var express = require('express');
+var app = express();
 
-app.listen(8080, () => console.log('Example app listening on port 3000!'))
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+
+
+
+
+// set the home page route
+app.get('/', function(req, res) {
+
+	// ejs render automatically looks in the views folder
+	res.send("index");
+});
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
